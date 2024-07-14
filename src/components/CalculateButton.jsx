f36b8391129b2b32e7c599ddecd08a55d43bac71
Lorenzo <lorenzo.castelletti98@gmail.com>
2024-07-14 11:29:16 +0200

    modificato effetto active calculate button

--- src/components/CalculateButton.jsx
+++ src/components/CalculateButton.jsx
@@ -5,7 +5,7 @@ function CalculateButton({ onClick, disabled }) {
     <button
       onClick={onClick}
       className={`w-full p-2 text-white rounded ${
-        disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'
+        disabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 active:bg-blue-900'
       }`}
       disabled={disabled}
     >
